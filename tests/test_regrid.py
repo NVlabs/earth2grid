02f4415f266a0f7e8ba80c932b0c14fa8567b237
Noah D. Brenowitz <nbren12@gmail.com>
2024-02-10 12:39:51 -0800

    add better test

--- tests/test_regrid.py
+++ tests/test_regrid.py
@@ -10,9 +10,16 @@ def test_regridder_healpix():
     dest = earth2grid.healpix.Grid(level=6, pixel_order=earth2grid.healpix.PixelOrder.XY)
     src = earth2grid.latlon.equiangular_lat_lon_grid(33, 64)
     regrid = earth2grid.get_regridder(src, dest)
-    x = torch.zeros(src.shape)
 
-    assert regrid(x).shape == dest.shape
+    def f(lat, lon):
+        lat = torch.from_numpy(lat)
+        lon = torch.from_numpy(lon)
+        return torch.cos(torch.deg2rad(lat)) * torch.sin(2 * torch.deg2rad(lon))
+
+    z = f(src.lat[:, None], src.lon)
+    z_regridded = regrid(z)
+    expected = f(dest.lat, dest.lon)
+    assert torch.allclose(z_regridded, expected, rtol=0.01)
 
 
 class TestBilinearInterpolateNonUniform(unittest.TestCase):
