1bda7ba187a058cafd73e15e8aa6c197c0f1ea94
Noah D. Brenowitz <nbren12@gmail.com>
2024-03-09 09:40:49 -0800

    cleanup wrapper

--- earth2grid/third_party/healpy_bare/healpix_bare_wrapper.cpp
+++ earth2grid/third_party/healpy_bare/healpix_bare_wrapper.cpp
@@ -7,30 +7,23 @@ int return_1(void) {
 }
 
 template<typename Func>
-torch::Tensor vectorize(Func func, int nside, torch::Tensor input) {
-  auto output = torch::empty_like(input);
-  auto accessor = input.accessor<int64_t, 1>();
-  auto out_accessor = output.accessor<int64_t, 1>();
+auto wrap(Func func) {
+  return [func](int nside, torch::Tensor input) {
+    auto output = torch::empty_like(input);
+    auto accessor = input.accessor<int64_t, 1>();
+    auto out_accessor = output.accessor<int64_t, 1>();
 
-  for (int64_t i = 0; i < input.size(0); ++i) {
-    out_accessor[i] = func(nside, accessor[i]); // Specify nside appropriately
-  }
+    for (int64_t i = 0; i < input.size(0); ++i) {
+      out_accessor[i] = func(nside, accessor[i]); // Specify nside appropriately
+    }
 
-  return output;
-}
-
-torch::Tensor ring2nest_wrapper(int nside, torch::Tensor input) {
-  return vectorize(&ring2nest, nside, input);
-}
-
-torch::Tensor nest2ring_wrapper(int nside, torch::Tensor input) {
-  return vectorize(&nest2ring, nside, input);
+    return output;
+  };
 }
 
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("return_1", &return_1, "First implementation.");
-  m.def("ring2nest", &ring2nest_wrapper, "Element-wise ring2nest conversion");
-  m.def("nest2ring", &nest2ring_wrapper, "Element-wise nest2ring conversion");
-
+  m.def("ring2nest", wrap(ring2nest), "Element-wise ring2nest conversion");
+  m.def("nest2ring", wrap(nest2ring), "Element-wise nest2ring conversion");
 }
