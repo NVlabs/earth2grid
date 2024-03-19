b0938673405839b69ded02d6898cf31c5627d8b2
Noah D. Brenowitz <nbren12@gmail.com>
2024-03-18 23:44:40 -0700

    add gnu compiler support
    was getting an error message with -Wno-error=c++11-narrowing on gnu
    compilers

--- setup.py
+++ setup.py
@@ -1,5 +1,40 @@
-from setuptools import Extension, setup
+import subprocess
+from typing import List
+
+from setuptools import setup
 from torch.utils import cpp_extension
+
+
+def get_compiler():
+    try:
+        # Try to get the compiler path from the CC environment variable
+        # If not set, it will default to gcc (which could be symlinked to clang or g++)
+        compiler = subprocess.check_output(["gcc", "--version"], universal_newlines=True)
+
+        if "clang" in compiler:
+            return "clang"
+        elif "g++" in compiler or "gcc" in compiler:
+            return "gnu"
+        else:
+            return "unknown"
+    except Exception as e:
+        print(f"Error detecting compiler: {e}")
+        return "unknown"
+
+
+compiler_type = get_compiler()
+extra_compile_args: List[str] = []
+
+if compiler_type == "clang":
+    print("Detected Clang compiler.")
+    # Additional settings or flags specific to Clang can be added here
+    extra_compile_args += ["-Wno-error=c++11-narrowing", "-Wno-c++11-narrowing"]
+elif compiler_type == "gnu":
+    print("Detected GNU compiler.")
+    # Additional settings or flags specific to G++ can be added here
+else:
+    print("Could not detect compiler or unknown compiler detected.")
+
 
 src_files = [
     "earth2grid/third_party/healpy_bare/healpix_bare_wrapper.cpp",
@@ -11,7 +46,7 @@ setup(
         cpp_extension.CppExtension(
             'earth2grid._healpix_bare',
             src_files,
-            extra_compile_args=["-Wno-error=c++11-narrowing", "-Wno-c++11-narrowing"],
+            extra_compile_args=extra_compile_args,
         )
     ],
     cmdclass={'build_ext': cpp_extension.BuildExtension},
