f9d84d4c5d4696ef0d5d90c04370ab78bed85019
Noah D. Brenowitz <nbren12@gmail.com>
2024-05-29 14:28:20 -0700

    fix pages deploy script

--- docs/push_docs.sh
+++ docs/push_docs.sh
@@ -10,7 +10,7 @@ function cleanup {
     echo "Cleaning up worktree"
     git worktree remove --force /tmp/earth2grid-pages
 }
-git worktree add pages /tmp/earth2grid-pages
+git worktree add /tmp/earth2grid-pages pages
 trap cleanup EXIT
 
 rsync -av --delete --exclude ".git" docs/_build/html/ /tmp/earth2grid-pages/
