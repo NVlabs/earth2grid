83d3b36eb67229a5531e49aac0aa9b5a16533b9c
Noah D. Brenowitz <nbrenowitz@nvidia.com>
2025-05-17 17:10:54 -0700

    fix sphinx-gallery page
    the thumbnail links no longer worked since myst_nb
    was rendering the ipynb and py files in the auto_examples folder,
    not the rst generated by sphinx-gallery
    
    Resolves #34

--- docs/conf.py
+++ docs/conf.py
@@ -20,27 +20,31 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Earth2-Grid'
-copyright = '2024, NVIDIA Research'
-author = 'NVIDIA Research'
+project = "Earth2-Grid"
+copyright = "2024, NVIDIA Research"
+author = "NVIDIA Research"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_nb',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_gallery.gen_gallery',
+    "myst_nb",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_gallery.gen_gallery",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "auto_examples/*.ipynb", "auto_examples/*.py"]
 
 myst_enable_extensions = [
     "colon_fence",
 ]
 nb_custom_formats = {".qmd": ["jupytext.reads", {"fmt": "qmd"}]}
+# don't compile sphinx-gallery
+nb_execution_excludepatterns = ["auto_examples/**"]
+# Only render .ipynb files outside auto_examples/
+
 
 # myst_nb configuration
 # myst_enable_extensions = [
@@ -90,18 +94,18 @@ if not os.path.exists(pyvista.FIGURE_PATH):
 
 # necessary when building the sphinx gallery
 pyvista.BUILDING_GALLERY = True
-os.environ['PYVISTA_BUILDING_GALLERY'] = 'true'
+os.environ["PYVISTA_BUILDING_GALLERY"] = "true"
 
 
 # sphinx gallery
 sphinx_gallery_conf = {
-    'examples_dirs': '../examples/sphinx_gallery/',  # path to your example scripts
-    'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
-    'filename_pattern': '',
+    "examples_dirs": "../examples/sphinx_gallery/",  # path to your example scripts
+    "gallery_dirs": "auto_examples",  # path to where to save gallery generated output
+    "filename_pattern": "",
     "image_scrapers": (DynamicScraper(), "matplotlib", PNGScraper()),
 }
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_static_path = ['_static']
-html_theme = 'sphinx_book_theme'
+html_static_path = ["_static"]
+html_theme = "sphinx_book_theme"
