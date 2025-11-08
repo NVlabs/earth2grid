aa85d408829c12f5108aad977c02bf7404637f19
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2025-11-07 16:34:15 -0800

    Add python bindings to get ring info to manipulate data in the ring layout like doing zonal interpolation, etc.

--- earth2grid/healpix_bare.py
+++ earth2grid/healpix_bare.py
@@ -15,7 +15,7 @@
 import torch
 
 from earth2grid import _healpix_bare
-from earth2grid._healpix_bare import ang2ring, corners, hpc2loc, hpd2loc, nest2hpd, nest2ring, ring2hpd, ring2nest
+from earth2grid._healpix_bare import ang2ring, corners, hpc2loc, hpd2loc, nest2hpd, nest2ring, ring2hpd, ring2nest, get_ring_info
 
 __all__ = [
     "pix2ang",
@@ -24,6 +24,7 @@ __all__ = [
     "nest2ring",
     "hpc2loc",
     "corners",
+    "get_ring_info",
 ]
 
 
